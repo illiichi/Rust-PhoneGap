76b031c246d54d9a932dbeff6d0cd26ee6fbb1f9
Zaicheng Qi <vmlinz@gmail.com>
2015-05-05 13:52:14 +0800

    Update java format

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -10,17 +10,13 @@ public class Hello extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("greet")) {
-
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
 
             return true;
-
         } else {
-            
             return false;
-
         }
     }
 }
